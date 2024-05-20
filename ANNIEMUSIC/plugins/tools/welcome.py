d540595e0c003c6869026a0fe91b64c69547aaee
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2024-05-20 23:51:39 +0530

    welcome.py

--- ANNIEMUSIC/plugins/tools/welcome.py
+++ ANNIEMUSIC/plugins/tools/welcome.py
@@ -65,7 +65,7 @@ def circle(pfp, size=(500, 500)):
     pfp.putalpha(mask)
     return pfp
 
-def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
+def welcomepic(pic, user, chatname, id, self.name, brightness_factor=1.3):
     background = Image.open("ANNIEMUSIC/assets/annie/AnnieNwel.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp)
