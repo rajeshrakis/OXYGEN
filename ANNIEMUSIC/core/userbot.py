0f2243f026b12854bea36c8ebf4ec6c3edb7d78a
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2024-05-20 14:40:03 +0530

    userbot.py

--- ANNIEMUSIC/core/userbot.py
+++ ANNIEMUSIC/core/userbot.py
@@ -51,9 +51,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("CHATTING_2024")
+                await self.one.join_chat("HeartBeat_Muzic")
               
-                await self.one.join_chat("+H5IrqVsgKYU3ODI1")
+                await self.one.join_chat("HeartBeat_Muzic")
             except:
                 pass
             assistants.append(1)
@@ -74,9 +74,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("+H5IrqVsgKYU3ODI1")
+                await self.two.join_chat("HeartBeat_Muzic")
                 
-                await self.one.join_chat("CHATTING_2024")
+                await self.one.join_chat("HeartBeat_Muzic")
             except:
                 pass
             assistants.append(2)
@@ -96,9 +96,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("+H5IrqVsgKYU3ODI1")
+                await self.three.join_chat("HeartBeat_Muzic")
                 
-                await self.one.join_chat("CHATTING_2024")
+                await self.one.join_chat("HeartBeat_Muzic")
             except:
                 pass
             assistants.append(3)
@@ -118,9 +118,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("+H5IrqVsgKYU3ODI1")
+                await self.four.join_chat("HeartBeat_Muzic")
                 
-                await self.one.join_chat("CHATTING_2024")
+                await self.one.join_chat("HeartBeat_Muzic")
             except:
                 pass
             assistants.append(4)
@@ -140,9 +140,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("+H5IrqVsgKYU3ODI1")
+                await self.five.join_chat("HeartBeat_Muzic")
                 
-                await self.one.join_chat("CHATTING_2024")
+                await self.one.join_chat("HeartBeat_Muzic")
             except:
                 pass
             assistants.append(5)
