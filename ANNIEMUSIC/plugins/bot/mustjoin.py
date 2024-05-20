89f98386e05f3c0a0d77e2a8ac1165afb95c82f5
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2024-05-20 11:37:46 +0530

    mustjoin.py

--- ANNIEMUSIC/plugins/bot/mustjoin.py
+++ ANNIEMUSIC/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from ANNIEMUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "BWANDARLOK"
+MUST_JOIN = "HeartBeat_Muzic"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
