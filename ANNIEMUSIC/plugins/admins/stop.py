d8a4f628d6235c0bfa707f8de0608d1d4bee124d
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2024-05-26 21:07:55 +0530

    stop.py

--- ANNIEMUSIC/plugins/admins/stop.py
+++ ANNIEMUSIC/plugins/admins/stop.py
@@ -10,7 +10,7 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    filters.command(["end", "stream"], prefixes=["/", "!"]) & filters.group & ~BANNED_USERS
+    filters.command(["end", "stream", "yplay"], prefixes=["/", "!"]) & filters.group & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
